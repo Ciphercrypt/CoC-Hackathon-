ab2df86d53832c9d7c0cc79c5b585ce7ea430db2
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-13 23:10:21 +0530

    added addEvent page

--- alumni/src/addpages/addjobs.js
+++ alumni/src/addpages/addjobs.js
@@ -19,6 +19,7 @@ import {
   InputAdornment,
   InputLabel,
   OutlinedInput,
+  TextareaAutosize,
   TextField,
   Typography,
   useMediaQuery,
@@ -40,13 +41,15 @@ import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 // ===========================|| FIREBASE - REGISTER ||=========================== //
 
-const addjobs = ({ ...others }) => {
+const AddJob = ({ ...others }) => {
   const theme = useTheme();
   const scriptedRef = useScriptRef();
   const matchDownSM = useMediaQuery(theme.breakpoints.down("md"));
   const customization = useSelector((state) => state.customization);
   const [showPassword, setShowPassword] = useState(false);
   const [checked, setChecked] = useState(true);
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
 
   const [strength, setStrength] = useState(0);
   const [level, setLevel] = useState();
@@ -76,54 +79,9 @@ const addjobs = ({ ...others }) => {
   return (
     <>
       <Grid container direction="column" justifyContent="center" spacing={2}>
+        
         <Grid item xs={12}>
-          <AnimateButton>
-            <Button
-              variant="outlined"
-              fullWidth
-              onClick={googleHandler}
-              size="large"
-              sx={{
-                color: "grey.700",
-                backgroundColor: theme.palette.grey[50],
-                borderColor: theme.palette.grey[100],
-              }}
-            >
-              <Box sx={{ mr: { xs: 1, sm: 2, width: 20 } }}>
-                <img
-                  src={Google}
-                  alt="google"
-                  width={16}
-                  height={16}
-                  style={{ marginRight: matchDownSM ? 8 : 16 }}
-                />
-              </Box>
-              Sign up with Google
-            </Button>
-          </AnimateButton>
-        </Grid>
-        <Grid item xs={12}>
-          <Box sx={{ alignItems: "center", display: "flex" }}>
-            <Divider sx={{ flexGrow: 1 }} orientation="horizontal" />
-            <Button
-              variant="outlined"
-              sx={{
-                cursor: "unset",
-                m: 2,
-                py: 0.5,
-                px: 7,
-                borderColor: `${theme.palette.grey[100]} !important`,
-                color: `${theme.palette.grey[900]}!important`,
-                fontWeight: 500,
-                borderRadius: `${customization.borderRadius}px`,
-              }}
-              disableRipple
-              disabled
-            >
-              OR
-            </Button>
-            <Divider sx={{ flexGrow: 1 }} orientation="horizontal" />
-          </Box>
+          
         </Grid>
         <Grid
           item
@@ -132,37 +90,27 @@ const addjobs = ({ ...others }) => {
           alignItems="center"
           justifyContent="center"
         >
-          <Box sx={{ mb: 2 }}>
-            <Typography variant="subtitle1">
-              Sign up with Email address
-            </Typography>
-          </Box>
+          
         </Grid>
       </Grid>
 
       <Formik
         initialValues={{
-          email: "",
-          password: "",
-          fname: "",
-          lname: "",
-          batch_year: "",
-          branch: "",
-          clg_id: "",
-          contact: "",
-          post: "",
+            title: "",
           company: "",
-          emp_type: "",
-          dob: "",
-          address:"",
+          location: "",
+          salary: "",
+          experience: "",
+          description: "",
+          job_type: "",
+          skills: "",
+          company_website: "",
+          company_email: "",
+          last_date_to_apply: "",
+          company_logo: "",
           submit: null,
         }}
         validationSchema={Yup.object().shape({
-          email: Yup.string()
-            .email("Must be a valid email")
-            .max(255)
-            .required("Email is required"),
-          password: Yup.string().max(255).required("Password is required"),
         })}
         onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
           try {
@@ -189,18 +137,19 @@ const addjobs = ({ ...others }) => {
           isSubmitting,
           touched,
           values,
+          setFieldValue
         }) => (
           <form noValidate onSubmit={handleSubmit} {...others}>
             <Grid container spacing={matchDownSM ? 0 : 2}>
               <Grid item xs={12} sm={6}>
                 <TextField
                   fullWidth
-                  label="First Name"
+                  label="Job Title"
                   margin="normal"
-                  name="fname"
+                  name="title"
                   type="text"
                   defaultValue=""
-                  value={ values.fname }
+                  value={ values.title }
                   onChange={(e) => 
                   handleChange(e)
                 }
@@ -210,10 +159,10 @@ const addjobs = ({ ...others }) => {
               <Grid item xs={12} sm={6}>
                 <TextField
                   fullWidth
-                  label="Last Name"
+                  label="Company"
                   margin="normal"
-                  name="lname"
-                  value={ values.lname }
+                  name="company"
+                  value={ values.company }
                   onChange={(e) => 
                   handleChange(e)
                 }
@@ -225,17 +174,18 @@ const addjobs = ({ ...others }) => {
             </Grid>
 
             <Grid container spacing={matchDownSM ? 0 : 2}>
+              
               <Grid item xs={12} sm={6}>
                 <TextField
                   fullWidth
-                  label="Batch Year"
+                  label="Location"
                   margin="normal"
-                  name="batch_year"
-                  value={ values.batch_year }
+                  name="location"
+                  type="text"
+                  value={ values.location }
                   onChange={(e) => 
                   handleChange(e)
                 }
-                  type="number"
                   defaultValue=""
                   sx={{ ...theme.typography.customInput }}
                 />
@@ -243,11 +193,11 @@ const addjobs = ({ ...others }) => {
               <Grid item xs={12} sm={6}>
                 <TextField
                   fullWidth
-                  label="branch"
+                  label="Salary"
                   margin="normal"
-                  name="branch"
+                  name="salary"
+                  value={values.salary}
                   type="text"
-                  value={ values.branch }
                   onChange={(e) => 
                   handleChange(e)
                 }
@@ -257,250 +207,45 @@ const addjobs = ({ ...others }) => {
               </Grid>
             </Grid>
 
-            <Grid container spacing={matchDownSM ? 0 : 2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="College ID"
-                  margin="normal"
-                  name="clg_id"
-                  value={ values.clg_id }
-                  onChange={(e) => 
-                  handleChange(e)
-                }
-                  type="Number"
-                  defaultValue=""
-                  sx={{ ...theme.typography.customInput }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Contact Number"
-                  margin="normal"
-                  name="contact"
-                  value={values.contact}
-                  type="text"
-                  onChange={(e) => 
-                  handleChange(e)
-                }
-                  defaultValue=""
-                  sx={{ ...theme.typography.customInput }}
-                />
-              </Grid>
-            </Grid>
 
             <Grid container spacing={matchDownSM ? 0 : 2}>
               <Grid item xs={12} sm={6}>
                 <InputLabel id="demo-simple-select-label">
-                  Employment Type
+                  Priority
                 </InputLabel>
                 <Select
                   labelId="demo-simple-select-label"
                   id="demo-simple-select"
-                  label="Age"
-                  onChange={handleChange}
+                  label="priority"
+                  onChange={(e) => setFieldValue('priority',e.target.value)}
+                  value={values.priority}
                 >
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  <MenuItem value={"Low"}>Low</MenuItem>
+                  <MenuItem value={"Medium"}>Medium</MenuItem>
+                  <MenuItem value={"High"}>High</MenuItem>
+                  <MenuItem value={"Very High"}>Very High</MenuItem>
                 </Select>
               </Grid>
               <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Current Post"
-                  margin="normal"
-                  name="post"
-                  type="text"
-                  onChange={(e) => 
-                  handleChange(e)
-                }
-                  defaultValue=""
-                  sx={{ ...theme.typography.customInput }}
-                />
+                 <TextField
+  id="datetime-local"
+  label="Select Date and Time"
+  type="datetime-local"
+  value={selectedDate}
+  onChange={(e) => setSelectedDate(e.target.value)}
+  InputLabelProps={{
+    shrink: true,
+  }}
+/>
+
               </Grid>
             </Grid>
 
-            <Grid container spacing={matchDownSM ? 0 : 2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Current Company"
-                  margin="normal"
-                  name="company"
-                  value={values.company}
-                  type="Text"
-                  onChange={(e) => 
-                  handleChange(e)
-                }
-                  defaultValue=""
-                  sx={{ ...theme.typography.customInput }}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  fullWidth
-                  label="Date Of Birth"
-                  margin="normal"
-                  name="dob"
-                  value={values.dob}
-                  type="Date"
-                  onChange={(e) => 
-                  handleChange(e)
-                }
-                  defaultValue=""
-                  sx={{ ...theme.typography.customInput }}
-                />
-              </Grid>
-            </Grid>
- <FormControl
-              fullWidth
-              error={Boolean(touched.address && errors.address)}
-              sx={{ ...theme.typography.customInput }}
-            >
-              <InputLabel htmlFor="outlined-adornment-email-register" >
-                Address
-              </InputLabel>
-              <OutlinedInput
-                rows={ 4 }
-                minRows={4}
-                id="outlined-adornment-email-register"
-                type="text"
-                value={values.address}
-                name="address"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                inputProps={{}}
-              />
-              {touched.address && errors.address && (
-                <FormHelperText
-                  error
-                  id="standard-weight-helper-text--register"
-                >
-                  {errors.address}
-                </FormHelperText>
-              )}
-            </FormControl>
+            
+      
 
-            <FormControl
-              fullWidth
-              error={Boolean(touched.email && errors.email)}
-              sx={{ ...theme.typography.customInput }}
-            >
-              <InputLabel htmlFor="outlined-adornment-email-register">
-                Email Address / Username
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-email-register"
-                type="email"
-                value={values.email}
-                name="email"
-                onBlur={handleBlur}
-                onChange={handleChange}
-                inputProps={{}}
-              />
-              {touched.email && errors.email && (
-                <FormHelperText
-                  error
-                  id="standard-weight-helper-text--register"
-                >
-                  {errors.email}
-                </FormHelperText>
-              )}
-            </FormControl>
 
-            <FormControl
-              fullWidth
-              error={Boolean(touched.password && errors.password)}
-              sx={{ ...theme.typography.customInput }}
-            >
-              <InputLabel htmlFor="outlined-adornment-password-register">
-                Password
-              </InputLabel>
-              <OutlinedInput
-                id="outlined-adornment-password-register"
-                type={showPassword ? "text" : "password"}
-                value={values.password}
-                name="password"
-                label="Password"
-                onBlur={handleBlur}
-                onChange={(e) => {
-                  handleChange(e);
-                  changePassword(e.target.value);
-                }}
-                endAdornment={
-                  <InputAdornment position="end">
-                    <IconButton
-                      aria-label="toggle password visibility"
-                      onClick={handleClickShowPassword}
-                      onMouseDown={handleMouseDownPassword}
-                      edge="end"
-                      size="large"
-                    >
-                      {showPassword ? <Visibility /> : <VisibilityOff />}
-                    </IconButton>
-                  </InputAdornment>
-                }
-                inputProps={{}}
-              />
-              {touched.password && errors.password && (
-                <FormHelperText
-                  error
-                  id="standard-weight-helper-text-password-register"
-                >
-                  {errors.password}
-                </FormHelperText>
-              )}
-            </FormControl>
-
-            {strength !== 0 && (
-              <FormControl fullWidth>
-                <Box sx={{ mb: 2 }}>
-                  <Grid container spacing={2} alignItems="center">
-                    <Grid item>
-                      <Box
-                        style={{ backgroundColor: level?.color }}
-                        sx={{ width: 85, height: 8, borderRadius: "7px" }}
-                      />
-                    </Grid>
-                    <Grid item>
-                      <Typography variant="subtitle1" fontSize="0.75rem">
-                        {level?.label}
-                      </Typography>
-                    </Grid>
-                  </Grid>
-                </Box>
-              </FormControl>
-            )}
-
-            <Grid container alignItems="center" justifyContent="space-between">
-              <Grid item>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      checked={checked}
-                      onChange={(event) => setChecked(event.target.checked)}
-                      name="checked"
-                      color="primary"
-                    />
-                  }
-                  label={
-                    <Typography variant="subtitle1">
-                      Agree with &nbsp;
-                      <Typography variant="subtitle1" component={Link} to="#">
-                        Terms & Condition.
-                      </Typography>
-                    </Typography>
-                  }
-                />
-              </Grid>
-            </Grid>
-            {errors.submit && (
-              <Box sx={{ mt: 3 }}>
-                <FormHelperText error>{errors.submit}</FormHelperText>
-              </Box>
-            )}
+            
 
             <Box sx={{ mt: 2 }}>
               <AnimateButton>
@@ -525,7 +270,7 @@ const addjobs = ({ ...others }) => {
   );
 };
 
-export default addJobs;
+export default AddJob;
 
 
 
