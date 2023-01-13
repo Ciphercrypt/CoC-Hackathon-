e00d4913877c50c857c920479efc09270e0bf7af
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-13 17:04:01 +0530

    added image upload to server

--- server/controllers/cloudController.js
+++ server/controllers/cloudController.js
@@ -1,42 +1,51 @@
-const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
-const dotenv = require('dotenv');
-dotenv.config()
+// const multer = require('multer');
+// const cloudinary = require('cloudinary').v2;
+// const dotenv = require('dotenv');
+// dotenv.config();
 
+// const storage = new CloudinaryStorage({
+// 	cloudinary : cloudinary,
+// 	params     : {
+// 		folder : 'test'
+// 	}
+// });
 
-// Cloudinary configuration
+// // Cloudinary configuration
+// cloudinary.config({
+// 	cloud_name : process.env.CLOUDINARY_NAME,
+// 	api_key    : process.env.CLOUDINARY_API_KEY,
+// 	api_secret : process.env.CLOUDINARY_API_SECRET
+// });
 
-// Multer configuration for file handling
-const storage = multer.memoryStorage();
+// // API to upload image to Cloudinary
+// module.exports = {
+// 	addImage : async (req, res, next) => {
+// 		try {
+// 			console.log('yetay ka');
+// 			console.log(process.env.CLOUDINARY_API_SECRET);
 
-// API to upload image to Cloudinary
-module.exports={
-addImage: async (req, res, next) => {
-  try {
-    console.log("yetay ka");
-    console.log(process.env.CLOUDINARY_API_SECRET);
+// 			const upload = multer({ storage: storage });
 
+// 			//console.log(req);
+// 			// Upload image to Cloudinary
+// 			const result = await cloudinary.uploader.upload(req.file.buffer.toString());
 
+// 			// Add image URL to the request body
+// 			req.body.photo_url = result.secure_url;
+// 			upload.single('image');
 
-    //console.log(req);
-    // Upload image to Cloudinary
-    const result = await cloudinary.uploader.upload(req.file.buffer.toString());
+// 			// Continue with the rest of the logic
 
-    // Add image URL to the request body
-    req.body.photo_url = result.secure_url;
-
-    // Continue with the rest of the logic
-   
-    // Send success response
-    console.log(result);
-    return res.status(200).json({
-      success: true,
-      message: "Image uploaded successfully",
-      response: result,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ success: false, message: error.message });
-  }
-},
-}
+// 			// Send success response
+// 			console.log(result);
+// 			return res.status(200).json({
+// 				success  : true,
+// 				message  : 'Image uploaded successfully',
+// 				response : result
+// 			});
+// 		} catch (error) {
+// 			console.log(error);
+// 			return res.status(400).json({ success: false, message: error.message });
+// 		}
+// 	}
+// };
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
+const multer = require('multer');
+const cloudinary = require('cloudinary').v2;
 const storage = multer.memoryStorage();
 
-const {addEvent,getEvent}=require('../controllers/eventController');
-const {addImage}=require('../controllers/cloudController');
+const { addEvent, getEvent } = require('../controllers/eventController');
+const { addImage } = require('../controllers/cloudController');
 
 const upload = multer({ storage });
-
 
 router.post('/addEvent', addEvent);
 router.get('/getEvent', getEvent);
 
-router.post("/addImage", upload.single("image"), addImage);
+// router.post("/addImage", upload.single("image"), addImage);
 
-module.exports = router
+module.exports = router;
