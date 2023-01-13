8573b64a3a6e6ce6252ea139838d653e0d6f1536
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2023-01-13 18:37:11 +0530

    added filter dilog

--- alumni/src/views/dashboard/Default/index.js
+++ alumni/src/views/dashboard/Default/index.js
@@ -14,6 +14,7 @@ import { gridSpacing } from 'store/constant';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
+import InstagramPost from './chart-data/PostComponent';
 
 // ==============================|| DEFAULT DASHBOARD ||============================== //
 
@@ -59,7 +60,17 @@ const Dashboard = () => {
 			<Grid item xs={12}>
 				<Grid container spacing={gridSpacing}>
 					<Grid item xs={12} md={8}>
-						<TotalGrowthBarChart isLoading={isLoading} />
+						{/* <TotalGrowthBarChart isLoading={isLoading} /> */}
+						<InstagramPost
+							username={'Avi Mohite'}
+							profile_picture={
+								'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png'
+							}
+							caption={'caption'}
+							branch="Computer"
+							year={2023}
+							media_url={'https://cdn.pixabay.com/photo/2016/05/24/16/48/mountains-1412683_1280.png'}
+						/>
 					</Grid>
 					<Grid item xs={12} md={4}>
 						<PopularCard isLoading={isLoading} />
